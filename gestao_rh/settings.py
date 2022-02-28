0aaac76b5f2171fdab2138f739a596e2355b9740
Fabricio Teodoro <fabricio.theodoro.ti@gmail.com>
2022-02-27 21:17:34 -0300

    App de Departamento

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.empresa',
-    'apps.funcionarios'
+    'apps.funcionarios',
+    'apps.departamentos',
 ]
 
 MIDDLEWARE = [
